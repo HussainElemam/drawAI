9b19a0b55fb913769d52011dc6e68e97a671e2d9
Mohammad Mohammad <126383137+moeMohammad@users.noreply.github.com>
2025-04-25 10:32:59 +0400

    Finished predict and enhance

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,33 @@
 import os
-
+from dotenv import load_dotenv
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
 from PIL import Image, ImageOps
 import io
+from google import genai
+from google.genai import types
 
+
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
+client = genai.Client(api_key=os.getenv('GOOGLE_API_KEY'))
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    file = request.files['image']
-    image = Image.open(file.stream)
-
-    # Do your thing and return the prediction
-    prediction = "Mountain"
-    print(jsonify({"prediction": prediction}))
-    return jsonify({"prediction": prediction})
+    try:
+        file = request.files['image']
+        image = Image.open(file.stream)
+        response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=[image, "What do you see in this image? respond with only one word as your prediction"]
+        )  
+        prediction = response.text.strip()
+        return jsonify({"prediction": prediction})
+        
+    except Exception as e:
+        print(f"Error in predict function: {str(e)}")  # Debug logging
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/api/enhance', methods=['POST'])
 def enhance():
@@ -24,7 +35,19 @@ def enhance():
     image = Image.open(file.stream)
 
     # Do your thing here and return proccessed image
-
+    text_input = "Can you generate a more refined version of this sketch keep the image sketch-like if possible and dont add too many details stick to the essence of the provided sketch"
+    response = client.models.generate_content(
+    model="gemini-2.0-flash-exp-image-generation",
+    contents=[text_input, image],
+    config=types.GenerateContentConfig(
+      response_modalities=['TEXT', 'IMAGE']
+        )
+    )
+    for part in response.candidates[0].content.parts:
+        if part.text is not None:
+            print(part.text)
+        elif part.inline_data is not None:
+            image = Image.open(io.BytesIO(part.inline_data.data))
     img_io = io.BytesIO()
     image.save(img_io, 'PNG')
     img_io.seek(0)

df11f6e73af8bf805bef8b26578e9d604eea125b
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-04-26 22:06:48 +0400

    change the api base url to the new deployed url

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,8 +1,11 @@
+const BASE = "https://drawai-production-4194.up.railway.app";
+// const BASE = "http://localhost:8080";
+
 async function getPrediction(blob) {
   const formData = new FormData();
   formData.append("image", blob, "canvas.png");
 
-  let response = await fetch("http://localhost:8080/api/predict", {
+  let response = await fetch(BASE + "/api/predict", {
     method: "POST",
     body: formData,
   });
@@ -17,7 +20,7 @@ async function getEnhanced(blob) {
   const formData = new FormData();
   formData.append("image", blob, "canvas.png");
 
-  const response = await fetch("http://localhost:8080/api/enhance", {
+  const response = await fetch(BASE + "/api/enhance", {
     method: "POST",
     body: formData,
   });

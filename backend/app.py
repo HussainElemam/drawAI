186c30b65b5871378aa9d4f2486d57b45fca96a3
Hussain Elemam <husseinhassanalemam@gmail.com>
2025-05-08 13:14:43 +0400

    try different preduction prompt

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ def predict():
         image = Image.open(file.stream)
         response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents=[image, "Predict what I am trying to draw, respond with only up to three words as your prediction"]
+        contents=[image, "Predict what I am trying to draw, respond with up to three words (less words is better)"]
         )  
         prediction = response.text.strip()
         return jsonify({"prediction": prediction})
